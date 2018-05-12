66235063ddc074444f062c15448524e18b7bd9ec
kathryn <kathrynchew@gmail.com>
2018-05-12 21:41:36 +0000

    Testing ingredients data cleaning using list comprehension

--- data/test_data_cleaner.py
+++ data/test_data_cleaner.py
@@ -14,22 +14,21 @@ for line in recipe_lines:
     # INGREDIENTS: Populate value as an array; split into 2 additional columns
     # of dict ingredient: qty (converted to grams) & text ingredients without
     # amounts
-    measure_names = ["cup", "cups", "teaspoon", "teaspoons", "tsp", "tsp.",
-                     "tablespoon", "tablespoons", "tbsp", "tbsp.", "ounce",
-                     "ounces", "oz", "oz.", "package", "pound", "pounds",
-                     "small", "large", "container", "stalk", "stalks", "pinch",
-                     "a pinch", "handful", "a handful", "few", "a few", "to",
-                     "plus", "of", "stick", "sticks", "can", "quart", "quarts",
-                     "dash", "pack", "sprig", "sprigs", "lb", "lb.", "bag",
-                     "liter", "bulk", "pieces"]
+    measure_names = set(["cup", "cups", "teaspoon", "teaspoons", "tsp", "tsp.",
+                         "tablespoon", "tablespoons", "tbsp", "tbsp.", "ounce",
+                         "ounces", "oz", "oz.", "package", "pound", "pounds",
+                         "small", "large", "container", "stalk", "stalks", "pinch",
+                         "handful", "a handful", "stick", "sticks", "can", "quart",
+                         "quarts", "dash", "pack", "sprig", "sprigs", "lb", "lb.",
+                         "bag", "liter", "pieces"])
 
-    descriptors = ["bulk", "store-bought", "finely", "diced", "chopped",
-                   "coarse", "coarsely", "freshly", "sliced", "loose", "of",
-                   "minced", "fresh", "a", "store-bought", "brand", "drops",
-                   "drop", "small"]
+    descriptors = set(["bulk", "store-bought", "finely", "diced", "chopped",
+                       "coarse", "coarsely", "freshly", "sliced", "loose", "of",
+                       "minced", "fresh", "a", "store-bought", "brand", "drops",
+                       "drop", "small", "of", "plus", "to", "few", "bulk"])
 
-    fractions = ["1/2", "1/4", "1/3", "3/4", "2/3", "1/8", "7/8", "1-",
-                 "1/2-", "1/4-", "1/2-inch", "1/4-inch", "8-ounce", "1-inch"]
+    fractions = set(["1/2", "1/4", "1/3", "3/4", "2/3", "1/8", "7/8", "1-",
+                 "1/2-", "1/4-", "1/2-inch", "1/4-inch", "8-ounce", "1-inch"])
 
     # TEXT INGREDIENTS (written as in original text)
     text_ingredients = line['ingredients']
@@ -41,25 +40,27 @@ for line in recipe_lines:
     for item in line['ingredients']:
         item = re.sub(r" ?\([^)]+\)", "", item)
         item = item.lower().split(" ")
-        for i in item:
-            if i.isnumeric():
-                item.remove(i)
-        print item
-        for i in item:
-            if i in measure_names:
-                item.remove(i)
-        print item
-        for i in item:
-            if i in descriptors:
-                item.remove(i)
-        print item
-        for i in item:
-            if i in fractions:
-                item.remove(i)
+        # for i in item:
+        #     if i.isnumeric():
+        #         item.remove(i)
+        # print item
+        # for i in item:
+        #     if i in measure_names:
+        #         item.remove(i)
+        # print item
+        # for i in item:
+        #     if i in descriptors:
+        #         item.remove(i)
+        # print item
+        # for i in item:
+        #     if i in fractions:
+        #         item.remove(i)
+        item = [i for i in item if ((i.isnumeric() is False) and (i not in measure_names and i not in descriptors and i not in fractions))]
         print item
         item = " ".join(item).rstrip().encode('utf-8')
         item = item.split(',')
         item = item[0]
+        print item
         ingredients_names.append(item)
         # qty_data.add(item)
 
