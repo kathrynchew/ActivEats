67cca7d7c0afa8166075061f1abe601cc4b6429b
kathryn <kathrynchew@gmail.com>
2018-06-07 05:15:18 +0000

    Built functions to query USDA API both by search term & ndbno number

--- test_usda_api/test_query.py
+++ test_usda_api/test_query.py
@@ -1,11 +1,26 @@
 import os
 import requests
+import re
+from pprint import pprint
 
 api_key = os.environ['USDA_KEY']
-query_key = '&api_key=' + api_key
-start_string = 'https://api.nal.usda.gov/ndb/search/?format=json&q='
 
-query_term = 'cinnamon%20basil'
 
-r = requests.get(start_string + query_term + query_key)
-results_json = r.json()
+def query_by_term(search_term):
+    """ Query the USDA Nutrition Data API by searching for a term or phrase """
+    search_term = re.sub(r"[^\w\s]", '', search_term)  # Remove all non-digit/letter characters
+    search_term = re.sub(r"\s+", '%20', search_term)  # Replace spaces with url-friendly '%20'
+
+    search_string = 'https://api.nal.usda.gov/ndb/search/?format=json&q={}&api_key={}'.format(search_term, api_key)
+    results = requests.get(search_string)
+
+    return results.json()
+
+
+def query_by_number(food_num):
+    """ Query the USDA Nutrition Data API by Nutritional Database Number (ndbno) """
+    search_number = 'https://api.nal.usda.gov/ndb/reports/?ndbno={}&type=b&format=json&api_key={}'.format(food_num, api_key)
+
+    results = requests.get(search_number)
+
+    return results.json()
